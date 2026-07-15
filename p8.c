c8a16531c08cee3b00b07823e7c0c7a9cbadfa9e
dipalipadhiyar137793-lab <dipali.padhiyar137793@marwadiuniversity.ac.in>
2026-07-15 13:11:14 +0530

    Add files via upload

---
+++ p8.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10],i,j,pos,temp,n=10;
+
+    printf("Enter 10 elements:\n");
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+
+    printf("Enter position to delete (1-10):");
+    scanf("%d",&pos);
+
+    for(i=pos-1;i<n-1;i++)
+        a[i]=a[i+1];
+
+        n--;
+
+    for(i=0;i<n-1;i++)
+        for(j=i+1;j<n;j++)
+            if(a[i]>a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+
+    printf("\n Array after deletion and sorting:\n");
+    for(i=0;i<n;i++)
+        printf("%d",a[i]);
+
+    return 0;
+}
