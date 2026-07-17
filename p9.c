a75577f69187a809b30b2fd4db2a9939435b933b
dipalipadhiyar137793-lab <dipali.padhiyar137793@marwadiuniversity.ac.in>
2026-07-17 13:12:06 +0530

    Update p9.c

--- p9.c
+++ p9.c
@@ -2,17 +2,17 @@
 
 int main()
 {
-    int a[10],i,j,temp;
+    int a[10],i,j,temp,n=10;
 
     printf("Enter 10 elements:\n");
     for(i=0;i<10;i++)
         scanf("%d",&a[i]);
 
-    for(i=0;i<9;i++)
+    for(i=0;i<n+1;i++)
     {
-        for(j=i+1;j<10;j++)
+        for(j=i+1;j<n;j++)
         {
-            if(a[i]>a[j])
+            if(a[i]<a[j])
             {
                 temp=a[i];
                 a[i]=a[j];
@@ -21,9 +21,11 @@ int main()
         }
     }
 
-    printf("\n Array in ascending order:\n");
-    for(i=0;i<10;i++)
+    printf("\n Array in desending order:\n");
+    for(i=0;i<n;i++)
         printf("%d",a[i]);
 
     return 0;
 }
+
+  
